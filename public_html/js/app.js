93ab21c29b6ae43a782b436ccd0ef9e555c083b8
Santosh Singh <mail1906@gmail.com>
2016-08-28 11:47:13 +0530

    basic chat console commit

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,4 +1,8 @@
 var socket = io();
 socket.on('connect', function () {
 	console.log("Connected to SERVER");
+});
+
+socket.on('message', function (message){
+	console.log(message.text);
 })
--- server.js
+++ server.js
@@ -4,10 +4,18 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.on('connection', function ()
-	{
-		console.log("user connected");
+io.on('connection', function (socket)
+{
+	console.log("user connected");
+
+	socket.on('message', function (message) {
+		socket.broadcast.emit('message',message);
 	});
+
+	socket.emit('message', {
+		text: "Hi Client"
+	});
+});
 
 app.use(express.static(__dirname + "/public_html"));
 http.listen(PORT, function () {
